a6d49e33e56b370d6f27c75aa0d65684378154c1
Arne Vansteenkiste <barnex@gmail.com>
2025-07-19 23:04:37 +0100

    draw a cat

--- webui/src/main.rs
+++ webui/src/main.rs
@@ -1,7 +1,6 @@
-use wasm_bindgen::JsCast;
-use wasm_bindgen::JsValue;
-use web_sys::CanvasRenderingContext2d;
-use web_sys::{HtmlCanvasElement, window};
+use wasm_bindgen::{prelude::Closure, JsCast, JsValue};
+use wasm_bindgen_futures::JsFuture;
+use web_sys::{console, window, CanvasRenderingContext2d, HtmlCanvasElement, HtmlImageElement, ImageBitmap};
 
 type JsResult<T> = Result<T, JsValue>;
 
@@ -18,7 +17,11 @@ async fn start() -> JsResult<()> {
 
     let ctx = canvas.get_context("2d").expect("context2d").unwrap().dyn_into::<CanvasRenderingContext2d>().unwrap();
     
-    ctx.fill_rect(4.0, 5.0, 20.0, 30.0);
+    //ctx.fill_rect(4.0, 5.0, 20.0, 30.0);
+    
+    let img = load_bitmap("kit3.png").await.expect("load img");
+    
+    ctx.draw_image_with_image_bitmap(&img, 0.0, 0.0).expect("draw");
 
     Ok(())
 }
@@ -28,6 +31,30 @@ fn say_hello() {
     let body = document.body().expect("Could not access document.body");
     let text_node = document.create_text_node("Hello, world from Vanilla Rust!");
     body.append_child(text_node.as_ref()).expect("Failed to append text");
+}
+
+
+pub async fn load_bitmap(path: &str) -> JsResult<ImageBitmap> {
+    console::log_1(&format!("load {path}").into());
+
+    // Load image element
+    let img = HtmlImageElement::new()?;
+
+    // Wait for image to load
+    let (load_tx, load_rx) = futures_channel::oneshot::channel();
+    let onload = Closure::once(Box::new(move || {
+        let _ = load_tx.send(());
+    }) as Box<dyn FnOnce()>);
+    img.set_onload(Some(onload.as_ref().unchecked_ref()));
+    onload.forget();
+    img.set_src(path);
+    load_rx.await.map_err(|_| JsValue::from_str("image load failed"))?;
+
+    // Create ImageBitmap from image element
+    let bitmap_promise = web_sys::window().unwrap().create_image_bitmap_with_html_image_element(&img)?;
+    let bitmap_jsvalue = JsFuture::from(bitmap_promise).await?;
+    let bitmap: ImageBitmap = bitmap_jsvalue.dyn_into()?;
+    Ok(bitmap)
 }
 
 #[track_caller]
