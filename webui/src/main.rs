bdd804713b47eca8f82ae5574878ef07857c0323
Arne Vansteenkiste <barnex@gmail.com>
2025-07-19 21:57:08 +0100

    async

--- webui/src/main.rs
+++ webui/src/main.rs
@@ -1,8 +1,14 @@
+use wasm_bindgen::JsValue;
 use web_sys::window;
+
+type JsResult<T> = Result<T, JsValue>;
 
 fn main() {
     console_error_panic_hook::set_once();
+    wasm_bindgen_futures::spawn_local(async { start().await.expect("main") })
+}
 
+async fn start() -> JsResult<()> {
     let document = window()
         .and_then(|win| win.document())
         .expect("Could not access the document");
@@ -10,4 +16,6 @@ fn main() {
     let text_node = document.create_text_node("Hello, world from Vanilla Rust!");
     body.append_child(text_node.as_ref())
         .expect("Failed to append text");
+
+    Ok(())
 }
