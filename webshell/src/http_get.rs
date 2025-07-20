3141206848627da1515b688049fa3a2f496f38bd
Arne Vansteenkiste <barnex@gmail.com>
2025-07-20 09:41:06 +0100

    factor http_get.rs

---
+++ webshell/src/http_get.rs
@@ -0,0 +1,30 @@
+use crate::*;
+
+// Trunk annoyingly returns index.html when a file is not found.
+// Work around this by recognizing a magic comment in index.html and returning an error instead.
+pub async fn http_get_with_trunk_hack(url: &str) -> JsResult<Vec<u8>> {
+    let bytes = http_get(url).await?;
+    if bytes.starts_with(b"<!DOCTYPE html> <!-- This comment identifies index.html DO NOT REMOVE -->") {
+        return Err(format!("GET {url}: got some HTML, presumably Trunk 404").into());
+    }
+    Ok(bytes)
+}
+
+// WARNING: ⚠️ Trunk serves index.html instead of 404 Not Found.
+// See `http_get_with_trunk_hack`.
+async fn http_get(url: &str) -> JsResult<Vec<u8>> {
+    let opts = RequestInit::new();
+    opts.set_method("GET");
+    opts.set_mode(web_sys::RequestMode::Cors);
+    let request = Request::new_with_str_and_init(url, &opts)?;
+
+    let response = JsFuture::from(window().fetch_with_request(&request)).await?.dyn_into::<Response>()?;
+    if !response.ok() {
+        return Err(JsValue::from_str(&format!("GET {url}: status {}: {}", response.status(), response.status_text())));
+    }
+
+    let array_buffer = JsFuture::from(response.array_buffer()?).await?;
+    let bytes = Uint8Array::new(&array_buffer).to_vec();
+
+    Ok(bytes)
+}
--- webshell/src/main.rs
+++ webshell/src/main.rs
@@ -11,6 +11,9 @@ use std::rc::Rc;
 
 type JsResult<T> = Result<T, JsValue>;
 
+mod http_get;
+use http_get::*;
+
 fn main() {
     console_error_panic_hook::set_once();
     console_log::init_with_level(log::Level::Info).expect("error initializing logger");
@@ -104,32 +107,6 @@ pub async fn load_bitmap(path: &str) -> JsResult<ImageBitmap> {
     Ok(bitmap)
 }
 
-// Trunk annoyingly returns index.html when a file is not found.
-// Work around this by recognizing a magic comment in index.html and returning an error instead.
-async fn http_get_with_trunk_hack(url: &str) -> JsResult<Vec<u8>> {
-    let bytes = http_get(url).await?;
-    if bytes.starts_with(b"<!DOCTYPE html> <!-- This comment identifies index.html DO NOT REMOVE -->") {
-        return Err(format!("GET {url}: got some HTML, presumably Trunk 404").into());
-    }
-    Ok(bytes)
-}
-
-async fn http_get(url: &str) -> JsResult<Vec<u8>> {
-    let opts = RequestInit::new();
-    opts.set_method("GET");
-    opts.set_mode(web_sys::RequestMode::Cors);
-    let request = Request::new_with_str_and_init(url, &opts)?;
-
-    let response = JsFuture::from(window().fetch_with_request(&request)).await?.dyn_into::<Response>()?;
-    if !response.ok() {
-        return Err(JsValue::from_str(&format!("GET {url}: status {}: {}", response.status(), response.status_text())));
-    }
-
-    let array_buffer = JsFuture::from(response.array_buffer()?).await?;
-    let bytes = Uint8Array::new(&array_buffer).to_vec();
-
-    Ok(bytes)
-}
 
 pub fn window() -> Window {
     web_sys::window().expect("window")
