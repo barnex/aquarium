ff032d4c887cbc8c45bbb4651e0491ac772cf8b9
Arne Vansteenkiste <barnex@gmail.com>
2025-07-25 20:55:10 +0100

    doc: storage/time

--- webshell/src/storage.rs
+++ webshell/src/storage.rs
@@ -1,6 +1,8 @@
+//! Persist data in browser storage.
 use crate::*;
 use base64::Engine as _;
 
+/// Serialize value to browser storage under given key.
 pub fn serialize<T>(key: &str, v: &T) -> JsResult<()>
 where
     T: Serialize,
@@ -9,18 +11,24 @@ where
     save_bytes(key, &bytes)
 }
 
-pub fn deserialize<T>(key: &str) -> JsResult<T> where T:DeserializeOwned{
-	let bytes = load_bytes(key)?;
-	let (v, _) = bincode::serde::decode_from_slice(&bytes, bincode::config::standard()).map_err(|e| format!("decode {key:?}: {e:?}"))?;
-	Ok(v)
+/// Deserialize from browser storage under given key.
+pub fn deserialize<T>(key: &str) -> JsResult<T>
+where
+    T: DeserializeOwned,
+{
+    let bytes = load_bytes(key)?;
+    let (v, _) = bincode::serde::decode_from_slice(&bytes, bincode::config::standard()).map_err(|e| format!("decode {key:?}: {e:?}"))?;
+    Ok(v)
 }
 
+/// Store data to browser storage under given key.
 pub fn save_bytes(key: &str, data: &[u8]) -> JsResult<()> {
     let storage = window().local_storage().unwrap().unwrap();
     let encoded = base64::engine::general_purpose::STANDARD.encode(data);
     storage.set_item(key, &encoded)
 }
 
+/// Load data from browser storage under given key.
 pub fn load_bytes(key: &str) -> JsResult<Vec<u8>> {
     let storage = window().local_storage().unwrap().unwrap();
     let s = storage.get_item(key)?.unwrap_or_else(|| "load_bytes {key:?}: not found".into());
--- webshell/src/time.rs
+++ webshell/src/time.rs
@@ -1,10 +1,7 @@
+//! Timekeeping.
 use web_sys::window;
 
 /// High-resolution time in seconds.
 pub fn now_secs() -> f64 {
-    window()
-        .unwrap()
-        .performance()
-        .unwrap()
-        .now() / 1000.0
+    window().unwrap().performance().unwrap().now() / 1000.0
 }
