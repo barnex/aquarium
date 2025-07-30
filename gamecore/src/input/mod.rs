d5597b96f050985c3db23772106612616bb8c398
Arne Vansteenkiste <barnex@gmail.com>
2025-07-30 08:22:58 +0100

    button.rs

--- gamecore/src/input/mod.rs
+++ gamecore/src/input/mod.rs
@@ -1,5 +1,7 @@
 mod inputs;
 mod keymap;
+mod button;
 
 pub use inputs::*;
 pub use keymap::*;
+pub use button::*;
--- gamecore/src/lib.rs
+++ gamecore/src/lib.rs
@@ -1,15 +1,15 @@
 pub(crate) mod prelude;
 
 mod game_commands;
+mod input;
 mod keybindings;
 mod output;
 mod state;
 mod tilemap;
-mod input;
 
 pub use game_commands::*;
+pub use input::*;
 pub use keybindings::*;
 pub use output::*;
 pub use state::*;
 pub use tilemap::*;
-pub use input::*;
