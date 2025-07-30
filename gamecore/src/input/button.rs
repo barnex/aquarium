8b477ce10c4733c056c391d9859e7cfa19b2ae4f
Arne Vansteenkiste <barnex@gmail.com>
2025-07-30 08:27:12 +0100

    cleanup button

--- gamecore/src/input/button.rs
+++ gamecore/src/input/button.rs
@@ -1,8 +1,11 @@
 use crate::prelude::*;
 
+/// Key code or mouse button. Backed by short string (`Str16`), copyable.
 #[derive(Serialize, Deserialize, Debug, Clone, Copy, Hash, PartialEq, Eq, PartialOrd, Ord)]
 pub struct Button(pub Str16);
 
+/// Constructor with compile-time size check (name fits `Str16`)
+/// E.g. `button!("Mouse1")`.
 #[macro_export]
 macro_rules! button {
     ($arg:tt) => {
@@ -11,26 +14,6 @@ macro_rules! button {
 }
 
 impl Button {
-    pub const SCREENSHOT: Self = Self(Str16::from_slice(b"F12\0\0\0\0\0\0\0\0\0\0\0\0\0"));
-    pub const MOUSE1: Self = Self(Str16::from_slice(b"Mouse1\0\0\0\0\0\0\0\0\0\0"));
-    pub const MOUSE2: Self = Self(Str16::from_slice(b"Mouse2\0\0\0\0\0\0\0\0\0\0"));
-    pub const FORWARD: Self = Self(Str16::from_slice(b"Forward\0\0\0\0\0\0\0\0\0"));
-    pub const BACKWARD: Self = Self(Str16::from_slice(b"Backward\0\0\0\0\0\0\0\0"));
-    pub const LEFT: Self = Self(Str16::from_slice(b"Left\0\0\0\0\0\0\0\0\0\0\0\0"));
-    pub const RIGHT: Self = Self(Str16::from_slice(b"Right\0\0\0\0\0\0\0\0\0\0\0"));
-    pub const CROUCH: Self = Self(Str16::from_slice(b"KeyZ\0\0\0\0\0\0\0\0\0\0\0\0"));
-    pub const JUMP: Self = Self(Str16::from_slice(b"Space\0\0\0\0\0\0\0\0\0\0\0"));
-    pub const ESC: Self = Self(Str16::from_slice(b"Escape\0\0\0\0\0\0\0\0\0\0"));
-    pub const TAB: Self = Self(Str16::from_slice(b"Tab\0\0\0\0\0\0\0\0\0\0\0\0\0"));
-    pub const SPACE: Self = Self(Str16::from_slice(b"Space\0\0\0\0\0\0\0\0\0\0\0"));
-    pub const DIGIT1: Self = Self(Str16::from_slice(b"Digit1\0\0\0\0\0\0\0\0\0\0"));
-    pub const DIGIT2: Self = Self(Str16::from_slice(b"Digit2\0\0\0\0\0\0\0\0\0\0"));
-    pub const DIGIT3: Self = Self(Str16::from_slice(b"Digit3\0\0\0\0\0\0\0\0\0\0"));
-    pub const DIGIT4: Self = Self(Str16::from_slice(b"Digit4\0\0\0\0\0\0\0\0\0\0"));
-    pub const DIGIT5: Self = Self(Str16::from_slice(b"Digit5\0\0\0\0\0\0\0\0\0\0"));
-    pub const DIGIT6: Self = Self(Str16::from_slice(b"Digit6\0\0\0\0\0\0\0\0\0\0"));
-    pub const DIGIT7: Self = Self(Str16::from_slice(b"Digit7\0\0\0\0\0\0\0\0\0\0"));
-    pub const DIGIT8: Self = Self(Str16::from_slice(b"Digit8\0\0\0\0\0\0\0\0\0\0"));
-    pub const DIGIT9: Self = Self(Str16::from_slice(b"Digit9\0\0\0\0\0\0\0\0\0\0"));
-    pub const DIGIT0: Self = Self(Str16::from_slice(b"Digit0\0\0\0\0\0\0\0\0\0\0"));
+    pub const MOUSE1: Self = button!("Mouse1");
+    pub const MOUSE2: Self = button!("Mouse2");
 }
