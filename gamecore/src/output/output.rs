3a61a4c39836c6b8f03704a24b2d2428b85bd310
Arne Vansteenkiste <barnex@gmail.com>
2025-07-31 21:35:25 +0100

    output: expose layers

--- gamecore/src/output/output.rs
+++ gamecore/src/output/output.rs
@@ -1,10 +1,25 @@
 use crate::prelude::*;
 
+/// The layer we draw the tilemap to.
+const L_TILES: u8 = 0;
+
+/// The layer we draw the sprites to.
+const L_SPRITES: u8 = 1;
+
+/// UI background layer (window fill etc).
+const L_UI_BG: u8 = 2;
+
+/// UI mid layer (text, buttons, ...).
+const L_UI: u8 = 3;
+
+/// UI foreground layer (selection markers etc).
+const L_UI_FG: u8 = 4;
+
 /// Scenegraph, sounds, etc. to output after a tick.
 /// Sent to the browser who will render it.
 #[derive(Default, Debug, PartialEq, Eq)]
 pub struct Output {
-    pub curr_layer: usize,
+    pub curr_layer: u8,
     pub layers: Vec<Layer>,
     pub debug: String,
 }
@@ -23,31 +38,52 @@ impl Output {
 
     pub fn clear(&mut self) {
         self.curr_layer = 0;
-        self.layers.clear();
+        self.layers.iter_mut().for_each(Layer::clear);
         self.debug.clear();
-        debug_assert!(self == &default());
     }
 
     pub fn push_sprite(&mut self, sprite: Sprite, pos: vec2i) {
-        self.curr_layer().sprites.push((sprite, pos));
+        self.push_sprite_l(self.curr_layer, sprite, pos);
+    }
+
+    pub fn push_sprite_l(&mut self, layer: u8, sprite: Sprite, pos: vec2i) {
+        self.layer(layer).sprites.push((sprite, pos));
     }
 
     pub fn push_line(&mut self, line: Line) {
-        self.curr_layer().lines.push(line);
+        self.push_line_l(self.curr_layer, line);
+    }
+
+    pub fn push_line_l(&mut self, layer: u8, line: Line) {
+        self.layer(layer).lines.push(line);
     }
 
     pub fn push_rect(&mut self, rect: Rectangle) {
-        self.curr_layer().rectangles.push(rect);
+        self.push_rect_l(self.curr_layer, rect);
+    }
+
+    pub fn push_rect_l(&mut self, layer: u8, rect: Rectangle) {
+        self.layer(layer).rectangles.push(rect);
     }
 
     pub fn new_layer(&mut self) {
         self.curr_layer += 1
     }
 
-    fn curr_layer(&mut self) -> &mut Layer {
-        while self.layers.len() <= self.curr_layer {
+    fn layer(&mut self, layer: u8) -> &mut Layer {
+        debug_assert!(layer <= 100, "too many layers");
+        while self.layers.len() <= layer.as_() {
             self.layers.push(Layer::default());
         }
-        &mut self.layers[self.curr_layer]
+        &mut self.layers[self.curr_layer as usize]
+    }
+}
+
+impl Layer {
+    fn clear(&mut self) {
+        self.sprites.clear();
+        self.lines.clear();
+        self.rectangles.clear();
+        debug_assert!(self == &Self::default(), "Layer::clear is correct");
     }
 }
