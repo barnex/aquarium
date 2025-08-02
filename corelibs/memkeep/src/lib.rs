127e129cab519faa3098cd3e5e4f53960d84736d
Arne Vansteenkiste <barnex@gmail.com>
2025-08-02 22:16:16 +0100

    memkeep/serde_support.rs

--- corelibs/memkeep/src/lib.rs
+++ corelibs/memkeep/src/lib.rs
@@ -1,8 +1,7 @@
-use serde::de::{SeqAccess, Visitor};
-use serde::{Deserialize, Deserializer, Serialize};
 use std::cell::{Cell, RefCell, UnsafeCell};
 use std::fmt::{Debug, Display};
 use std::marker::PhantomData;
+mod serde_support;
 
 /// A memory arena indexed by generational indices,
 /// that hands out references (`&T`).
@@ -14,64 +13,6 @@ pub struct MemKeep<T> {
     // TODO: grow storage
 }
 
-impl<T> Serialize for MemKeep<T>
-where
-    T: Serialize,
-{
-    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
-    where
-        S: serde::Serializer,
-    {
-        self.enumerate().collect::<Vec<_>>().serialize(serializer)
-    }
-}
-
-impl<'de, T> Deserialize<'de> for MemKeep<T>
-where
-    T: Deserialize<'de>,
-{
-    fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
-    where
-        D: Deserializer<'de>,
-    {
-        struct MyWrapperVisitor<T> {
-            marker: PhantomData<fn() -> MemKeep<T>>,
-        }
-
-        impl<'de, T> Visitor<'de> for MyWrapperVisitor<T>
-        where
-            T: Deserialize<'de>,
-        {
-            type Value = MemKeep<T>;
-
-            fn expecting(&self, formatter: &mut std::fmt::Formatter) -> std::fmt::Result {
-                formatter.write_str("a list of items for MyWrapper")
-            }
-
-            fn visit_seq<A>(self, mut seq: A) -> Result<Self::Value, A::Error>
-            where
-                A: SeqAccess<'de>,
-            {
-                let mut m = MemKeep::new();
-
-                while let Some((id, v)) = seq.next_element()? {
-                    m._insert_at(id, v)
-                }
-
-                for (i, slot) in m.storage.iter_mut().enumerate().rev() {
-                    if !slot.not_deleted.get() {
-                        debug_assert!(slot.value.get_mut().is_none());
-                        m.freelist.get_mut().push(i as u32);
-                    }
-                }
-
-                Ok(m)
-            }
-        }
-
-        deserializer.deserialize_seq(MyWrapperVisitor { marker: PhantomData })
-    }
-}
 
 struct Slot<T> {
     generation: Cell<u32>,
@@ -199,24 +140,6 @@ pub struct Id {
     generation: u32,
 }
 
-impl Serialize for Id {
-    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
-    where
-        S: serde::Serializer,
-    {
-        (self.index, self.generation).serialize(serializer)
-    }
-}
-
-impl<'de> Deserialize<'de> for Id {
-    fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
-    where
-        D: Deserializer<'de>,
-    {
-        let (index, generation) = <(u32, u32)>::deserialize(deserializer)?;
-        Ok(Id { index, generation })
-    }
-}
 
 impl Debug for Id {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
