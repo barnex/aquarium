ed2d7da8892a97de131e6ca0f7fb64f0416571f9
Arne Vansteenkiste <barnex@gmail.com>
2025-08-04 08:50:17 +0100

    more g:G

--- gamecore/src/drawing.rs
+++ gamecore/src/drawing.rs
@@ -6,54 +6,55 @@ impl G {
         debug_assert!(self.viewport_size != vec2::ZERO);
         // Note: ⚠️ UI already rendered (may consume input events)
 
-        self.draw_tilemap(out);
-        self.draw_buildings(out);
-        self.draw_pawns(out);
-        self.draw_cursor(out);
-        self.draw_selection(out);
-        self.draw_routes(out);
+        let g = self;
+        draw_tilemap(g, out);
+        draw_buildings(g, out);
+        draw_pawns(g, out);
+        draw_cursor(g, out);
+        draw_selection(g, out);
+        draw_routes(g, out);
+    }
+}
+
+pub fn draw_tilemap(g: &G, out: &mut Output) {
+    for (idx, mat) in g.tilemap.enumerate_all() {
+        out.push_sprite(L_TILES, mat.sprite(), idx.pos() - g.camera_pos);
+    }
+}
+
+pub fn draw_buildings(g: &G, out: &mut Output) {
+    for building in &g.buildings {
+        out.push_sprite(L_SPRITES, building.typ.sprite(), building.tile * TILE_ISIZE - g.camera_pos);
+    }
+}
+
+fn draw_pawns(g: &G, out: &mut Output) {
+    for pawn in g.pawns.iter() {
+        out.push_sprite(L_SPRITES, pawn.typ.sprite(), pawn.tile.pos() - g.camera_pos);
+    }
+}
+
+fn draw_cursor(g: &G, out: &mut Output) {
+    let sprite = match g.ui.active_tool {
+        Tool::Pointer => sprite!("grid24"),
+        Tool::Tile(typ) => typ.sprite(),
+        Tool::Pawn(typ) => typ.sprite(),
+    };
+    out.push_sprite(L_SPRITES, sprite, g.mouse_tile().pos() - g.camera_pos);
+    out.push_sprite(L_SPRITES, sprite!("grid24"), g.mouse_tile().pos() - g.camera_pos);
+}
+
+fn draw_selection(g: &G, out: &mut Output) -> Option<()> {
+    let sel = g.pawns.get(g.selected.get()?)?;
+    out.push_rect(L_SPRITES, Rectangle::new(sel.bounds().translated(-g.camera_pos), RGBA::BLUE).with_fill(RGB::BLUE.with_alpha(64)));
+    OK
+}
+
+fn draw_routes(g: &G, out: &mut Output) -> Option<()> {
+    let pawn = g.pawns.get(g.selected.get()?)?;
+    if !pawn.is_at_destination() {
+        out.push_line(L_SPRITES, Line::new(pawn.center(), pawn.dest.pos() + TILE_ISIZE / 2).with_color(RGB::WHITE.with_alpha(128)).translated(-g.camera_pos));
     }
 
-    pub fn draw_tilemap(&self, out: &mut Output) {
-        for (idx, mat) in self.tilemap.enumerate_all() {
-            out.push_sprite(L_TILES, mat.sprite(), idx.pos() - self.camera_pos);
-        }
-    }
-
-    pub fn draw_buildings(&self, out: &mut Output) {
-        for building in &self.buildings {
-            out.push_sprite(L_SPRITES, building.typ.sprite(), building.tile * TILE_ISIZE - self.camera_pos);
-        }
-    }
-
-    fn draw_pawns(&self, out: &mut Output) {
-        for pawn in self.pawns.iter() {
-            out.push_sprite(L_SPRITES, pawn.typ.sprite(), pawn.tile.pos() - self.camera_pos);
-        }
-    }
-
-    fn draw_cursor(&self, out: &mut Output) {
-        let sprite = match self.ui.active_tool {
-            Tool::Pointer => sprite!("grid24"),
-            Tool::Tile(typ) => typ.sprite(),
-            Tool::Pawn(typ) => typ.sprite(),
-        };
-        out.push_sprite(L_SPRITES, sprite, self.mouse_tile().pos() - self.camera_pos);
-        out.push_sprite(L_SPRITES, sprite!("grid24"), self.mouse_tile().pos() - self.camera_pos);
-    }
-
-    fn draw_selection(&self, out: &mut Output) -> Option<()> {
-        let sel = self.pawns.get(self.selected.get()?)?;
-        out.push_rect(L_SPRITES, Rectangle::new(sel.bounds().translated(-self.camera_pos), RGBA::BLUE).with_fill(RGB::BLUE.with_alpha(64)));
-        OK
-    }
-
-    fn draw_routes(&self, out: &mut Output) -> Option<()> {
-        let pawn = self.pawns.get(self.selected.get()?)?;
-		if !pawn.is_at_destination(){
-			out.push_line(L_SPRITES, Line::new(pawn.center(), pawn.dest.pos() + TILE_ISIZE/2).with_color(RGB::WHITE.with_alpha(128)).translated(-self.camera_pos));	
-		}
-
-        OK
-    }
+    OK
 }
