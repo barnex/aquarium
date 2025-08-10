6e2d4b625de62235e4b442cc44dbccee1f919b8d
Arne Vansteenkiste <barnex@gmail.com>
2025-08-10 12:50:35 +0100

    tests hide ui to avoid accidental clicking.

--- gamecore/src/tests/test_setup.rs
+++ gamecore/src/tests/test_setup.rs
@@ -7,7 +7,9 @@ use crate::prelude::*;
 
 static LOGGER_INIT: OnceLock<()> = OnceLock::new();
 
-pub(crate) fn init_logging() {
+/// Initialize `env_logger` for testing.
+/// Idempotent.
+pub(crate) fn init_test_logging() {
     LOGGER_INIT.get_or_init(|| {
         env_logger::builder()
             .is_test(true) // nicer formatting for `cargo test`
@@ -18,22 +20,26 @@ pub(crate) fn init_logging() {
     });
 }
 
-/// A small test world.
+/// A small test world with some features.
 pub(crate) fn small_world() -> G {
-    init_logging();
-    let mut g = G::new(vec2(64, 32));
+    let mut g = test_world(vec2(64, 32));
 
     for (x, y) in cross(2..5, 3..7) {
         g.tilemap.set(vec2(x, y), Tile::Water);
     }
-
     for (x, y) in cross(6..9, 2..5) {
         g.tilemap.set(vec2(x, y), Tile::Block);
     }
-
     g.tilemap.set(vec2(4, 4), Tile::Sand);
-
     g.viewport_size = vec2(480, 320);
+    g
+}
 
+/// Base test world.
+/// Test settings enabled. No features.
+fn test_world(size: vec2u16) -> G {
+    init_test_logging();
+    let mut g = G::new(size);
+    g.ui.hidden = true;
     g
 }
