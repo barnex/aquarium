b75f7aa7a63cc286d2a934db1ab50fb86f978a96
Arne Vansteenkiste <barnex@gmail.com>
2025-08-13 13:36:39 +0100

    CSet tweaks

--- gamecore/src/c_set.rs
+++ gamecore/src/c_set.rs
@@ -1,6 +1,7 @@
 use itertools::Itertools as _;
 use serde::{Deserialize, Serialize};
 use std::cell::RefCell;
+use std::fmt::Debug;
 use std::hash::Hash;
 
 type HashSet<T> = fnv::FnvHashSet<T>;
@@ -14,29 +15,40 @@ impl<T: Eq + Hash + Copy> CSet<T> {
         Self::default()
     }
 
-    pub fn remove(&self, v: &T) {
-        self.0.borrow_mut().remove(v);
+    /// Like HashSet::remove().
+    pub fn remove(&self, v: &T) -> bool {
+        self.0.borrow_mut().remove(v)
     }
 
+    /// Like HashSet::insert().
     pub fn insert(&self, v: T) {
         self.0.borrow_mut().insert(v);
     }
 
+    /// Like HashSet::iter().
     pub fn iter(&self) -> impl Iterator<Item = T> {
         // ⚠️ TODO: without cloning.
         self.0.borrow().iter().copied().collect_vec().into_iter()
     }
 
+    /// Like HashSet::len.
     pub fn len(&self) -> usize {
         self.0.borrow().len()
     }
 
+    /// Like HashSet::is_empty.
     pub fn is_empty(&self) -> bool {
         self.0.borrow().is_empty()
     }
 
+    /// Like HashSet::clear.
     pub fn clear(&self) {
         self.0.borrow_mut().clear();
+    }
+
+    /// Like HashSet::contains.
+    pub fn contains(&self, v: T) -> bool {
+        self.0.borrow().contains(&v)
     }
 }
 
@@ -49,5 +61,11 @@ impl<T: Eq + Hash + Copy> FromIterator<T> for CSet<T> {
 impl<T: Eq + Hash + Copy> Default for CSet<T> {
     fn default() -> Self {
         Self(Default::default())
+    }
+}
+
+impl<T: Eq + Hash + Copy + Debug> Debug for CSet<T> {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        self.0.borrow().fmt(f)
     }
 }
