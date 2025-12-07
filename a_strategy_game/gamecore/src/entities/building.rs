e9f8e72b9da1eec1bd10cf96dcff0de27452556d
Arne Vansteenkiste <barnex@gmail.com>
2025-12-07 14:23:04 +0000

    cleanup

--- a_strategy_game/gamecore/src/entities/building.rs
+++ a_strategy_game/gamecore/src/entities/building.rs
@@ -38,7 +38,17 @@ impl BuildingTyp {
         }
     }
 
-    pub fn default_workers(&self) -> (PawnTyp, usize) {
+    /// Index in Building.resrouces and max capacity.
+    /// 0 unused :(
+    fn _resource_metadata(self) -> [Option<(usize, u16)>; ResourceTyp::COUNT] {
+        match self {
+            BuildingTyp::HQ => [None, Some((0, 100)), Some((1, 100))],
+            BuildingTyp::Farm => [None, Some((0, 20)), None],
+            BuildingTyp::Quarry => [None, None, Some((0, 30))],
+        }
+    }
+
+    pub fn default_workers(self) -> (PawnTyp, usize) {
         match self {
             BuildingTyp::HQ => (PawnTyp::Crablet, 2),
             BuildingTyp::Farm => (PawnTyp::Crablet, 1),
@@ -48,6 +58,7 @@ impl BuildingTyp {
 }
 
 impl Building {
+    //-------------------------------------------------------------------------------- spawn/init
     pub fn new(typ: BuildingTyp, tile: impl Into<Vector<i16, 2>>) -> Self {
         Self {
             id: default(),
@@ -78,6 +89,7 @@ impl Building {
         }
     }
 
+    //-------------------------------------------------------------------------------- buildinf function
     pub fn is_full(&self) -> bool {
         self.resource_slots().all(|(_, slot, cap)| slot.get() >= cap)
     }
@@ -133,13 +145,14 @@ impl Building {
 
     /// Current number and maximum capacity for given resource.
     pub fn resource_slot(&self, res: ResourceTyp) -> Option<(&Cel<u16>, u16)> {
-        let (i, cap) = Self::_resource_metadata(self.typ)[res as usize]?;
+        let (i, cap) = self.typ._resource_metadata()[res as usize]?;
         Some((&self.resources[i], cap))
     }
 
     /// Iterate Resource type, current amount, and maximum capacity.
     pub fn resource_slots(&self) -> impl Iterator<Item = (ResourceTyp, &Cel<u16>, u16)> {
-        Self::_resource_metadata(self.typ)
+        self.typ
+            ._resource_metadata()
             .into_iter()
             .enumerate()
             .filter_map(|(r, v)| v.map(|(i, cap)| (r, i, cap)))
@@ -148,22 +161,13 @@ impl Building {
 
     pub fn iter_resources(&self) -> impl Iterator<Item = (ResourceTyp, u16)> {
         // TODO: reverse index instead of linear search
-        Self::_resource_metadata(self.typ)
+        self.typ
+            ._resource_metadata()
             .into_iter()
             .enumerate()
             .filter_map(|(r, v)| v.map(|(i, _cap)| (r, i)))
             .map(|(r, i)| (ResourceTyp::try_from_primitive(r as u8).unwrap(), i))
             .map(|(r, i)| (r, self.resources[i as usize].get()))
-    }
-
-    /// Index in Building.resrouces and max capacity.
-    /// 0 unused :(
-    fn _resource_metadata(typ: BuildingTyp) -> [Option<(usize, u16)>; ResourceTyp::COUNT] {
-        match typ {
-            BuildingTyp::HQ => [None, Some((0, 100)), Some((1, 100))],
-            BuildingTyp::Farm => [None, Some((0, 20)), None],
-            BuildingTyp::Quarry => [None, None, Some((0, 30))],
-        }
     }
 
     /// Building size in tiles. E.g. 3x3.
