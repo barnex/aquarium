56f307137756bda90a5c87408042dceb8dc67d94
Arne Vansteenkiste <barnex@gmail.com>
2025-12-23 17:28:45 +0000

    cleanup

--- a_strategy_game/gamecore/src/entities/mod.rs
+++ a_strategy_game/gamecore/src/entities/mod.rs
@@ -1,19 +1,15 @@
 mod building;
-mod building3;
 mod building_typ;
 mod entities;
 mod pawn;
-mod pawn3;
 mod pawn_typ;
 mod route;
 mod team;
 
 pub use building::*;
 pub use building_typ::*;
-pub use building3::*;
 pub use entities::*;
 pub use pawn::*;
 pub use pawn_typ::*;
-pub use pawn3::*;
 pub use route::*;
 pub use team::*;
